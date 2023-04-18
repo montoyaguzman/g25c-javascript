728821252fef24a90cb8979dd9bb401ea26241bf
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-04-17 21:13:14 -0600

    feat: definir e inicializar variables

--- 3-javascript/3.2-js-imported/js/index.js
+++ 3-javascript/3.2-js-imported/js/index.js
@@ -1,3 +1,12 @@
+// mostrar un mensaje en la consola
 console.log("hola mundo!");
+
+/* definir e inicializar una variable para despues mostrarla en la consola */
 var fatherName = "jose";
 console.log(fatherName);
+
+/* definir una variable = crearla */
+var motherName;
+
+/* iniciliazar una variable */
+motherName = "adela";
